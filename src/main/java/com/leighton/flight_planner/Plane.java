1d829a14014dff917fee6fcf0f51f88ef94ec249
Nelson Sproul <nelson.sproul@gmail.com>
2023-03-21 16:12:52 -0700

    Add a comment.

--- src/main/java/com/leighton/flight_planner/Plane.java
+++ src/main/java/com/leighton/flight_planner/Plane.java
@@ -3,6 +3,7 @@ package com.leighton.flight_planner;
 public class Plane {
     String planeType;
     public Plane(String planeType) {
+        // set fields and initialize
         this.planeType = planeType;
     }
 }
